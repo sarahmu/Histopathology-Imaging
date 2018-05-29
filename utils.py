6f8156175bf6d4c7ebe0276a4a9a9cdd2562055b
Chris Lin <chris522229197@gmail.com>
2018-05-29 00:49:51 +0000

    Add methods for showing and saving images

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import time
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 def local_clock(shift=-25200):
 	return time.asctime(time.localtime(time.time() + shift))
@@ -19,3 +22,44 @@ def preprocess(img):
 def postprocess(img):
     img = (img + 1) / 2 * 255.0
     return img
+
+def show_images(images, post_process=False, save=False, filepath=None):
+    N, H, W, C = images.shape
+    sqrtN = int(np.ceil(np.sqrt(N)))
+    if post_process:
+        images = postprocess(images)
+    images = images.astype(np.uint8)
+    fig = plt.figure(figsize=(sqrtN, sqrtN))
+    gs = gridspec.GridSpec(sqrtN, sqrtN)
+    gs.update(wspace=0.05, hspace=0.05)
+    
+    for i in range(N):
+        img = images[i,:,:,:]
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        if C == 1:
+            plt.imshow(img.reshape((H, W)), cmap='gray')
+        else:
+            plt.imshow(img)
+    if save and filepath != None:
+        fig.savefig(filepath)
+    plt.close(fig)
+    return
+
+def save_image(image, filepath, post_process=False, width=4, height=4):
+    H, W, C = image.shape
+    if post_process:
+        image = postprocess(image)
+    image = image.astype(np.uint8)
+    fig = plt.figure(figsize=(width, height))
+    plt.axis('off')
+    if C == 1:
+        plt.imshow(image.reshape((H, W)), camp='gray')
+    else:
+        plt.imshow(image)
+    fig.savefig(filepath)
+    plt.close(fig)
+    return
