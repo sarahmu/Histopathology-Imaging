f746f67357f0ac41371a7520fd0a942618d56e51
Chris Lin <chris522229197@gmail.com>
2018-05-31 02:47:50 +0000

    Training on whole dataset

--- train_gan.sh
+++ train_gan.sh
@@ -1,5 +1,5 @@
-python train_gan.py -t /home/shared/chris/Histopathology-Imaging/testing/ \
--v /home/shared/chris/Histopathology-Imaging/testing/ \
--o /home/shared/chris/Histopathology-Imaging/gan_testing/ \
--d 2e-4 -g 2e-4 -b 0.5 -r 16000 -n 30 -l l2 \
--s 16 -e -a -c /gpu:0 -i 30
+python train_gan.py -t /home/shared/data/center_4/patches_256/datasets/train/ \
+-v /home/shared/data/center_4/patches_256/datasets/val/ \
+-o /home/shared/results/gan_0530/ \
+-d 2e-4 -g 2e-4 -b 0.5 -r 16000 -n 50 -l l2 \
+-s 16 -e -a -c /gpu:0 -i 500
