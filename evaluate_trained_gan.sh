5796547a0a97d217d50e5b40d76b3f479f05b9cf
Chris Lin <chris522229197@gmail.com>
2018-06-01 05:06:46 +0000

    Evaluate trained GAN with L2 loss

--- evaluate_trained_gan.sh
+++ evaluate_trained_gan.sh
@@ -1,5 +1,5 @@
-python evaluate_trained_gan.py -m '/home/shared/chris/Histopathology-Imaging/gan_testing/trained_sess/Dlr=0.0002_Glr=0.0002_beta1=0.5_reg=16000.0_loss=l2_epoch_29.meta' \
--c '/home/shared/chris/Histopathology-Imaging/gan_testing/trained_sess/Dlr=0.0002_Glr=0.0002_beta1=0.5_reg=16000.0_loss=l2_epoch_29' \
--e /home/shared/chris/Histopathology-Imaging/testing/ \
--o /home/shared/chris/Histopathology-Imaging/gan_testing/test_eval/ \
--i 60
+python evaluate_trained_gan.py -m '/home/shared/results/gan_0530/trained_sess/Dlr=0.0002_Glr=0.0002_beta1=0.5_reg=16000.0_loss=l2_epoch_19.meta' \
+-c '/home/shared/results/gan_0530/trained_sess/Dlr=0.0002_Glr=0.0002_beta1=0.5_reg=16000.0_loss=l2_epoch_19' \
+-e /home/shared/data/center_4/patches_256/datasets/test/ \
+-o /home/shared/results/gan_0530/test_results/ \
+-i 10000
