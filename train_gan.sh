3693020835182e5b6a6196f65136c35fbe0e16ab
Chris Lin <chris522229197@gmail.com>
2018-06-01 01:21:56 +0000

    Experiment with L1 loss

--- train_gan.sh
+++ train_gan.sh
@@ -1,5 +1,5 @@
 python train_gan.py -t /home/shared/data/center_4/patches_256/datasets/train/ \
 -v /home/shared/data/center_4/patches_256/datasets/val/ \
--o /home/shared/results/gan_0530/ \
--d 2e-4 -g 2e-4 -b 0.5 -r 16000 -n 50 -l l2 \
+-o /home/shared/results/gan_0531/ \
+-d 2e-4 -g 2e-4 -b 0.5 -r 16000 -n 30 -l l1 \
 -s 16 -e -a -c /gpu:0 -i 500
