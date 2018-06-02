671ebc539a6dfd6f25e376f58978e3e62169d5e2
Chris Lin <chris522229197@gmail.com>
2018-06-02 17:11:29 +0000

    Unet with L1 loss

--- train_gan.sh
+++ train_gan.sh
@@ -1,5 +1,5 @@
 python train_gan.py -t /home/shared/data/center_4/datasets/train/ \
 -v /home/shared/data/center_4/datasets/val/ \
--o /home/shared/unet_results/0602_l2/ \
--d 2e-4 -g 2e-4 -b 0.5 -r 1 -n 30 -l l2 \
+-o /home/shared/unet_results/0602_l/ \
+-d 2e-4 -g 2e-4 -b 0.5 -r 1 -n 30 -l l1 \
 -s 16 -e -a -c /gpu:0 -i 500
